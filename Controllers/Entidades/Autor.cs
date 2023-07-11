1b1391ef56097ef89e44e221f0eacd57801c4a14
Francisco Arevalo <franciscoarevalo151@gmail.com>
2023-07-11 16:03:24 -0300

    new feats

--- Controllers/Entidades/Autor.cs
+++ Controllers/Entidades/Autor.cs
@@ -1,9 +1,13 @@
-﻿namespace WebApplication1.Controllers.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using WebApplication1.Validators;
+
+namespace WebApplication1.Controllers.Entidades
 {
     public class Autor
     {
-        public int Id { get; set; } 
-        public string Nombre { get; set; }    
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")][PrimeraLetraMayuscula] public string Nombre { get; set; }    
         public List<Libro> Libros { get; set; }
 
     }
